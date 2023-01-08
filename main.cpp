f759c8dc0ce2fcd0709b8c44d025f47af40f0ec3
Pedro <65189764+Pedrox42@users.noreply.github.com>
2023-01-08 00:30:28 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,14 +3,28 @@
 #include <unistd.h>
 #include <csignal>
 #include <vector>
+#include <pthread.h>
 
 using namespace std;
+
+void *thread_loop(void *arg)
+{
+    sleep(20);
+    pthread_detach(pthread_self());
+    return NULL;
+}
 
 int main()
 {
     pid_t parentId = getpid();
+
     vector<pid_t> processos(0);
     vector<string> nomes(0);
+
+    processos.push_back(parentId);
+    nomes.push_back("Processo Principal");
+
+    //coloca algo no vetor
 
     while(true)
     {
@@ -19,8 +33,11 @@ int main()
         cout << "-------------------------------" << endl;
         cout << "Eescolha uma opção: " << endl;
         cout << "[1] Criar um novo processo" << endl;
-        cout << "[2] Listar processos ativos" << endl;
+        cout << "[2] Listar processos" << endl;
         cout << "[3] matar um processo" << endl;
+        cout << "[4] Criar uma nova thread" << endl;
+        cout << "[5] Listar threads de um processo" << endl;
+        cout << "[6] Encerrar thread" << endl;
 
         cin >> escolha;
 
@@ -33,6 +50,7 @@ int main()
                 cin >> nome;
                 nomes.push_back(nome);
                 processos.push_back(fork());
+
                 break;
             }
 
@@ -60,18 +78,52 @@ int main()
                 cout << "Digite o id do processo que deseja remover: ";
                 cin >> processoId;
                 kill(processoId, 9);
+                break;
             }
 
-            default :
+            case '4':
             {
+                pthread_t threadId;
+                pthread_create(&threadId, NULL, thread_loop, NULL);
+                cout << "thread criada com sucesso!" << endl;
+                break;
+            }
+
+            case '5':
+            {
+                cout << "Digite o id do processo que deseja listar as threads: ";
+                pid_t processoEscolhidoId;
+
+                cin >> processoEscolhidoId;
+
+                string command = "ps -p " + to_string(processoEscolhidoId) + " -T";
+                system(command.c_str());
+                break;
+            }
+
+            case '6':
+            {
+                pthread_t threadParaSerEncerrada;
+                cout << "Digite o id da thread do processo principal que deseja encerrar: ";
+                cin >> threadParaSerEncerrada;
+                pthread_kill(threadParaSerEncerrada, 9);
+                break;
+            }
+
+            default:
+            {
+                cout << "Por favor Digite um valor válido.";
                 break;
             }
         }
 
         if(parentId != getpid())
         {
-            while(true){
-                continue;
+            while(true)
+            {
+                pthread_t threadId;
+                pthread_create(&threadId, NULL, thread_loop, NULL);
+                sleep(10);
             }
         }
     }
